967e811d409d87cc85b07d447ff2da0c03dd3692
ken shoufer <kshoufer@gmail.com>
2012-08-19 19:54:14 -0700

    The first commit

---
+++ BasicQuiz(v3)/SplashPage.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BasicQuiz_v3_
+{
+    public partial class SplashPage : Form
+    {
+        bool bolMatch = false;
+        int intUserID;
+        string strUserName;
+        string strUserPassword;
+        
+        public SplashPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadUserProfileTable();
+            
+            if (!bolMatch)
+            {
+                MessageBox.Show("The User Name and/or Passord Does Not Match" + "\n" + "Try Again");
+                txtPassword.Text = "";
+                txtUserName.Text = "";
+                txtUserName.Focus();
+            }
+        }
+
+        private void ReadUserProfileTable()
+        {
+            System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection();
+            conn.ConnectionString = @"Data Source=KEN-HP\SQLSERVER2008R2;Initial Catalog=quiz1;Integrated Security=True";
+
+            try
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(
+                    "SELECT * FROM UserProfile;",
+                    conn);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        strUserName = (string)reader[1];
+                        strUserPassword = (string)reader[2];
+                        if (strUserName.ToString() == txtUserName.Text && strUserPassword == txtPassword.Text)
+                        {
+                            bolMatch = true;
+                            intUserID = (int)reader[0];
+                            QuestionPage qp = new QuestionPage(intUserID);
+                            qp.Visible = true;
+                            this.Hide();
+                        }
+                    }// end while
+                }
+                else
+                {
+                    Console.WriteLine("No rows found");
+                }// end if (reader.HasRows)
+
+                reader.Close();
+
+            }// end try
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to connect to UserProfile");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            
+        } // end ReadUserProfileTable
+
+    }
+}
---
+++ BasicQuiz(v3)/SplashPage.designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BasicQuiz_v3_
+{
+    partial class SplashPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Basic Quiz(V3)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(29, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter Here";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(26, 90);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(63, 13);
+            this.lblUserName.TabIndex = 2;
+            this.lblUserName.Text = "User Name:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(26, 131);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(95, 90);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(95, 128);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // SplashPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "SplashPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashPage";
+            this.Load += new System.EventHandler(this.SplashPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
